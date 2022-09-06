98624ede8af4858e817cb169912875cf54a67b47
Hemanth2465 <111737771+Hemanth2465@users.noreply.github.com>
2022-09-06 12:26:00 +0530

    Hemanth created ROUNDROBIN.c

---
+++ OSLAB/EXP-1/ROUNDROBIN.c
@@ -0,0 +1,7 @@
+ 
+/* Name Of the Candidate: L. Ashok
+   Roll No: 204G1A0501
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
